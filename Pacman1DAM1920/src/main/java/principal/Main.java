fb6ed408faf28c4ebe501d0d4970fe93ee092890
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-02-11 08:55:00 +0100

    Creació de proyecto en blanco
    Creació de proyecto en blanco

---
+++ Pacman1DAM1920/src/main/java/principal/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ * Clase principal del pacman de 1ºDAM 19-20
+ * @author Miguel Páramos
+ */
+public class Main {
+
+    /**
+     * función inicial que lanza una partida de pacman
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
