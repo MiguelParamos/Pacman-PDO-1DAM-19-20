b890c384b980eb52352f20375ab65b841101702a
adri323 <adrianmat775@gmail.com>
2020-02-12 10:02:30 +0100

    clase Personaje(incompleta)
    todo relacion con subclases

---
+++ Pacman1DAM1920/src/main/java/principal/Personaje.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author cenec
+ */
+public class Personaje extends Elemento{
+    
+    private boolean estaVivo;
+    private int velocidad;
+
+  
+    
+    
+    public boolean isEstaVivo() {
+        return estaVivo;
+    }
+
+    public void setEstaVivo(boolean estaVivo) {
+        this.estaVivo = estaVivo;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+
+    
+    
+    public Personaje(boolean estaVivo, int velocidad, int x, int y, char s) {
+        super(x, y, s);
+        this.estaVivo = estaVivo;
+        this.velocidad = velocidad;
+    }  
+    
+}
