e70be57cb8c3f0292cb4dd593de6ed6e261ac2a1
Rompe-Ralph <57758368+Rompe-Ralph@users.noreply.github.com>
2020-02-13 12:31:01 +0100

    Update Pacman.java

--- Pacman1DAM1920/src/main/java/principal/Pacman.java
+++ Pacman1DAM1920/src/main/java/principal/Pacman.java
@@ -45,6 +45,7 @@ public class Pacman extends Elemento{
      * @return 
      */
     public int getNumeroVidas(){
+            
         return this.numeroVidas;
     }
     
@@ -100,18 +101,23 @@ public class Pacman extends Elemento{
     //FUNCIONES
     
     /**
-     *  Constructor de Pacman con sus posicion
-     * 
-     * @param posX posicion X del pacman
+     *  Constructor de Pacman con sus posicion generada aleatoriamente
+     *  
+     * @param posX posicion X del pacman 
      * @param posY posicion Y del pacman
      */
     public Pacman (int posX, int posY){
-            
+        //Genera la posicion aleatoria X
+        int posicionIncialX = Math.random()*getMapa().length();
+        //Genera la posicion aleatoria Y
+        int posicionIncialY = Math.random()*getMapa()[0].length();
     }
     /**
      *  Constructor de Pacman
      */
     public void Pacman(){
+        
+        
         
     }
     /**
@@ -149,10 +155,13 @@ public class Pacman extends Elemento{
         
     }
     /**
-     * Funcion al morir Pacman
+     * Funcion al morir Pacman resta una vida, y vuelve al pacman a la posición inicial
+     *
      */
     public void morir(){
-        
+        this.numeroVidas--;
+        this.posX = this.posicionInicialX;
+        this.posY = this.posicionInicialY;
     }
     
     /**
