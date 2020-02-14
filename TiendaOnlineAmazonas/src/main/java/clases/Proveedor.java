7ff476b6be5a3912865298a611231d662e4552a7
cristicr <ccristi.cr@outlook.com>
2020-02-14 10:17:35 +0100

    getter y setter proveedor

--- TiendaOnlineAmazonas/src/main/java/clases/Proveedor.java
+++ TiendaOnlineAmazonas/src/main/java/clases/Proveedor.java
@@ -14,15 +14,14 @@ public class Proveedor {
     private String nombre; //nombre del proveedor
     private String contacto; //contacto del proveedor
     // private Productos[] productos;
-    private int volumenMax; //volumen maximo del proveedor
+    private int volumenMax; //volumen maximo de ventas
 
-    
-    
     /**
      * Constructor de la clase proveedor con todos sus datos.
+     *
      * @param nombre nombre del proveedor
      * @param contacto contacto del proveedor
-     * @param volumenMax  volumen maximo del proveedor
+     * @param volumenMax volumen maximo de las ventas
      */
     public Proveedor(String nombre, String contacto, int volumenMax) {
         this.nombre = nombre;
@@ -32,31 +31,53 @@ public class Proveedor {
 
     /**
      * getter de nombre
+     *
      * @return nombre del proveedor
      */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * setter de nombre
+     *
+     * @param nombre nombre del provedor
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * getter de contacto
+     *
+     * @return datos de contactos del proveedor
+     */
     public String getContacto() {
         return contacto;
     }
 
+    /**
+     *setter de contacto
+     * @param contacto datos de contactos del proveedor
+     */
     public void setContacto(String contacto) {
         this.contacto = contacto;
     }
 
+    /**
+     * getter de volumenMax
+     * @return volumen maximo de ventas
+     */
     public int getVolumenMax() {
         return volumenMax;
     }
 
+    /**
+     * setter de volumenMax
+     * @param volumenMax  volumen maximo de ventas
+     */
     public void setVolumenMax(int volumenMax) {
         this.volumenMax = volumenMax;
     }
 
-    
 }
